1167ca4f7dc879b254d8153403008462dbe42aec
Can <oarkon@hotmail.com>
2017-12-01 09:08:39 +0300

    Add project page

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+})
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
 });
